bfa093e76ac93e78da1810e21e2cf59670590f68
oriaror <farraxovfarraxov@gmail.com>
2024-04-25 19:06:21 +0400

    fix2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,37 +1,41 @@
-import Operator from "@/components/OperatorCard.tsx/Operator"
-import Content from "@/components/ContentComponent/Content"
-import { Paragr } from "@/components/ContentComponent/ContentStyles"
+import Operator from "@/components/OperatorCard.tsx/Operator";
+import Content from "@/components/ContentComponent/Content";
+import { Paragr } from "@/components/ContentComponent/ContentStyles";
 
 interface IData {
-  id: number,
-  name: string,
-  img: string,
-  slug: string
+  id: number;
+  name: string;
+  img: string;
+  slug: string;
 }
 
-
 async function getData() {
-  const res = await fetch(process.env.DB_HOST + '/api/operators', { cache: 'no-store' })
+  const res = await fetch(process.env.DB_HOST + "/api/operators", {
+    cache: "no-store",
+  });
   if (!res.ok) {
-    throw new Error('Error')
+    throw new Error("Error");
   }
-  return res.json()
+  return res.json();
 }
 
 export default async function Home() {
-  const data: IData[] = await getData()
+  const data: IData[] = await getData();
 
   return (
     <>
       <Paragr>Выберите оператора</Paragr>
       <Content>
-        {data.map((item) =>
+        {data.map((item) => (
           <Operator
+            key={item.id}
             id={item.id}
             name={item.name}
             img={item.img}
-            slug={item.slug} />)}
+            slug={item.slug}
+          />
+        ))}
       </Content>
     </>
-  )
+  );
 }
