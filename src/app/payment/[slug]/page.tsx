b6c0775ab6179d64b1718188b108780390b9b8c8
oriaror <farraxovfarraxov@gmail.com>
2024-04-25 19:30:19 +0400

    fix3

--- src/app/payment/[slug]/page.tsx
+++ src/app/payment/[slug]/page.tsx
@@ -2,7 +2,7 @@ import PaymentForm from "@/components/PaymentForm/PaymentForm";
 import { notFound } from "next/navigation";
 
 async function getData(slug: string) {
-  const req = await fetch(`http://localhost:3000/api/operators/${slug}`);
+  const req = await fetch(process.env.DB_HOST + `/api/operators/${slug}`);
   if (req.status === 404) {
     notFound();
   } else {
